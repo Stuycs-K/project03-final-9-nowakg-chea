574c4e9b7949c13ae17fa39eed8106617343b18f
gnowak40 <gnowak40@stuy.edu>
2024-01-04 15:32:00 -0500

    worked on player and role struct

--- player.h
+++ player.h
@@ -1,28 +1,32 @@
-#include 'server.h'
+#include "server.h"
+#include "role.h"
 
 #ifdef PLAYER_H
 #define PLAYER_H
 
 #define MAX_PLAYERS 10
-#define ALIVE 1;
-#define DEAD 0;
+#define ALIVE 1
+#define DEAD 0
+
 
 struct playerMessage{
     struct player player;
     char[BUFFER_SIZE] message;
-}
+};
 
-//should we put destination in sendMessage or in the struct? 
-
-void sendMessage(struct playerMessage data, struct playerGroup[MAX_PLAYERS]);
-
-struct player {
+struct player{
     char[BUFFER_SIZE] name;
     struct role role;
     int dead;
     //file descriptor for select stuff
-}
+};
+
+
+//struct playerGroup[MAX_PLAYERS] will be the "chats" and will be handled and routed by server
+
+void sendMessage(struct playerMessage data);
+
 
 
 //chats will just made as a list of the stuct players
-//Mafia team will just be a list of struct players, and then when you send a message you can send a message 
+//Mafia team will just be a list of struct players, and then when you send a message you can send a message
