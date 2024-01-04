2f983228be4c999899cddfcd0933ae676862b3c8
Andrew Che <ache40@stuy.edu>
2024-01-04 14:40:00 -0500

    Added support for coloring text

--- input.h
+++ input.h
@@ -3,7 +3,12 @@
 
 #ifndef INPUT_H
 #define INPUT_H
+
+#define COLOR_RED 31
+#define COLOR_GRAY 37
+#define COLOR_RESET 0
 void clearScreen();
 void query(char* message, char* response, int buffSize);
 void moveCursor(int x, int y);
+int colorText(int color);
 #endif
